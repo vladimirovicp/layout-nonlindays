37308d17cea1a6bfeb743ac67c969fe80e44987b
vladimirovicp <vladimirovicp70@gmail.com>
2023-08-14 19:07:17 +0400

    settings: Webpack

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # layout-nonlindays
+
+* npm init -y
+* npm i -D webpack webpack-cli
+* npm i -D html-webpack-plugin
+* npm i -D webpack-dev-server
+* npm i -D filemanager-webpack-plugin
+* npm i -D pug pug-loader
+* npm i -D sass-loader postcss-loader postcss-preset-env css-loader style-loader sass
+* npm i -D mini-css-extract-plugin
+* npm i -D cross-env
+* npm i -D @babel/core @babel/preset-env babel-loader
+* npm i -D clean-webpack-plugin
+* npm i -D copy-webpack-plugin
+* npm i -D gh-pages
+* npm i -D html-loader
+* npm i -D html-webpack-plugin
---
+++ webpack.config.js
@@ -0,0 +1,123 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].${ext}`;
+const assetFilename = '[path][name][ext]';
+
+const pathDirFile = isDev ? 'app' : 'dist';
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, pathDirFile),
+        filename: `./js/${filename('js')}`,
+        assetModuleFilename: assetFilename,
+        clean: true,
+    },
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory: path.join(__dirname, 'src'),
+        },
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3000,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            favicon: "./favicon.ico",
+            template: path.resolve(__dirname, 'src/index.pug'),
+            filename: "index.html",
+        }),
+
+
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`,
+        }),
+
+        // new FileManagerPlugin({
+        //     events: {
+        //         onStart: {
+        //             delete: ['dist'],
+        //         },
+        //         onEnd: {
+        //             copy: [
+        //                 {
+        //                     source: path.join(__dirname, 'src/img/sprite-images'),
+        //                     destination: path.join(__dirname, 'dist/img/sprite-images'),
+        //                 },
+        //                 {
+        //                     source: path.join(__dirname, 'src/img/favicon'),
+        //                     destination: path.join(__dirname, 'dist/img/favicon'),
+        //                 },
+        //             ],
+        //         },
+        //     },
+        // }),
+
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true,
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: (resourcePath, context) => {
+                            return path.relative(path.dirname(resourcePath), context) + '../';
+                        },
+                    }
+                },
+                    "css-loader",
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            // {
+            //     test: /\.(ico)$/i,
+            //     type: 'asset/resource',
+            // },
+            //
+            // {
+            //     test: /\.(woff2?|eot|ttf|otf)$/i,
+            //     type: 'asset/resource',
+            // },
+        ]
+    }
+};
