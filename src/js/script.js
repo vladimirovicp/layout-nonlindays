46e7b9c496edfe013f2b0f60795697983238790e
Хмыров А.М <a.hmyrov-saratov@mail.ru>
2023-08-21 01:26:01 +0400

    desctop header

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,8 @@
-/* "use strict";
+"use strict";
 
 let burgerMenu = document.querySelector('.header__burger-wrap');
 let burgerNav = document.querySelector('.header__navigation-wrap');
 
 burgerMenu.addEventListener('click', () =>{
-    burgerMenu.classList.toggle('_burger');
-    let burgerIcon = burgerMenu.childNodes[1].classList.toggle('_burger-icon');
-    
-    burgerNav.classList.toggle('_active');
-}) */
+    burgerNav.classList.add('_active');
+})
