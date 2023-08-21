76e77d06402d944d308990eb0e89fbded0b46a36
Хмыров А.М <a.hmyrov-saratov@mail.ru>
2023-08-22 00:20:22 +0400

    add: index content

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,12 @@
 
 let burgerMenu = document.querySelector('.header__burger-wrap');
 let burgerNav = document.querySelector('.header__navigation-wrap');
+let burgerClose = document.querySelector('.header__navigation-burger');
 
 burgerMenu.addEventListener('click', () =>{
     burgerNav.classList.add('_active');
+})
+
+burgerClose.addEventListener('click', () =>{
+    burgerNav.classList.remove('_active');
 })
