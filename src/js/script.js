66351a3f1f8a98874ecca20724a55877810ce0dc
Хмыров А.М <a.hmyrov-saratov@mail.ru>
2023-08-23 22:39:07 +0400

    test about errors

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,18 @@
 let burgerMenu = document.querySelector('.header__burger-wrap');
 let burgerNav = document.querySelector('.header__navigation-wrap');
 let burgerClose = document.querySelector('.header__navigation-burger');
+let navigationList = document.querySelector('.header__navigation-list');
 
 burgerMenu.addEventListener('click', () =>{
     burgerNav.classList.add('_active');
 })
+
+let navItem = navigationList.children;
+for (let i = 0; i < navItem.length; i++){
+    navItem[i].addEventListener('click', ()=>{
+        burgerNav.classList.remove('_active');
+    })
+}
 
 burgerClose.addEventListener('click', () =>{
     burgerNav.classList.remove('_active');
